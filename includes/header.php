2ee0f15b038d434a9b9cdacfd8d16281c2e56c3e
Julian Litcanu <email@julian.id.au>
2014-06-24 13:52:50 +1000

    Adding structure

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>Ivy on the park</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="/includes/css/style.css" media="screen">
+</head>
+<body>
+
+</body>
+</html>
---
+++ interior.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>      </title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="" media="screen">
+<script type="javascript" src=""></script>
+</head>
+<body>
+
+</body>
+</html>
