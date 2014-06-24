c4c0317e2bd054cb321716113e195c622c0debf9
Julian Litcanu <email@julian.id.au>
2014-06-24 16:37:27 +1000

    added middle content

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,45 @@
+<div class="border">.</div>
+<div class="footer">
+	<div class="footer-table">
+		<div class="floor-plans">
+<h3>FLOOR PLANS</h3>
+<a href="#">CLICK TO VIEW</a>
+<p>G.01 | G.02 | G.03</p>
+<p>G.04 | G.05 | G.06</p>
+<p>1.01 | 1.02 | 1.03</p>
+<p>1.04 | 1.05 | 1.06</p>
+<p>PENTHOUSE 1</p>
+<p>PENTHOUSE 2</p>
+
+		</div>
+		<div class="roi-form">
+			<h2>Register your interst now</h2>
+		</div>
+		<div class="contact">
+			<h2>Contact Us</h2>
+			<div>
+				<div class="contact-img-left">
+					<img src="includes/images/contact-1.png" alt="">
+				</div>
+				<div class="contact-text-right">
+					<p>Bill Kyriakopoulos</p>
+					<p>0419 008 008</p>
+					<p>bill.k@loverealestate.com.au</p>
+				</div>
+			</div>
+			<div>
+				<div class="contact-img-left">
+					<img src="includes/images/contact-2.png">
+				</div>
+				<div class="contact-text-right">
+					<p>Bill Kyriakopoulos</p>
+					<p>0419 008 008</p>
+					<p>bill.k@loverealestate.com.au</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
     <!-- Bootstrap core JavaScript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="includes/js/bootstrap.min.js"></script>
